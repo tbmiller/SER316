6f22ab0f94a3d28674a3de04a03bdbfbb35767e5
amehlhas <a.mehlhase@asu.edu>
2025-10-10 14:36:39 -0700

    Add maxAttempts constant and game over state

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -1,10 +1,14 @@
 public class GameEngine {
+    private static final int MAX_ATTEMPTS = 10;
+
     private final int min;
     private final int max;
     private int target;
     private int attempts;
     private boolean gameWon;
     private boolean userQuit;
+    private boolean gameOver;
+
 
     public GameEngine(int min, int max) {
         this.min = min;
@@ -12,6 +16,7 @@ public class GameEngine {
         this.attempts = 0;
         this.gameWon = false;
         this.userQuit = false;
+        this.gameOver = false;
         reset();
     }
 
@@ -39,6 +44,7 @@ public class GameEngine {
         attempts = 0;
         gameWon = false;
         userQuit = false;
+        gameOver = false;
     }
 
     public boolean isGameWon() {
@@ -47,10 +53,16 @@ public class GameEngine {
 
     public boolean hasUserQuit() {
         return userQuit;
+    public boolean isGameOver() {
+        return gameOver;
     }
 
     public int getAttempts() {
         return attempts;
+    }
+
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS;
     }
 
     public int getMin() {
